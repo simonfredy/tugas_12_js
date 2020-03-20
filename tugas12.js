5245758e4a4600a536e5f99664754bea4c4a9377
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-20 18:46:22 +0700

    Tugas 12 JS

---
+++ tugas12.js
@@ -0,0 +1,19 @@
+function tugasDuabelas() {
+    var buah = ['Pisang', 'Jeruk', 'Apel', 'Mangga'];
+    console.log(buah);
+
+    //Pop
+    var buahPop = buah.pop();
+    return buah;
+}
+
+function tugasJs(){
+    var buah2 = ['Pisang', 'Jeruk', 'Apel'];
+
+    //Shift
+    var buahShift = buah2.shift();
+    return buah2;
+}
+
+console.log(tugasDuabelas());
+console.log(tugasJs());
